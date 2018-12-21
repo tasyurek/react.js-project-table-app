8a48ea0d2d1556b3e011ea8167d11daaf235e90a
onurcantasyurek <onurcantasyurek@gmail.com>
2018-12-21 22:49:39 +0300

    Create Project Summary

--- src/components/projects/ProjectList.js
+++ src/components/projects/ProjectList.js
@@ -1,68 +1,13 @@
 import React from "react";
+import ProjectSummary from "./ProjectSummary";
 
 const ProjectList = () => {
   return (
     <div className="project-list section">
       <h2>Projects</h2>
-      <div className="card z-depth-1 project-summary grey darken-3 white-text">
-        <div className="card-content">
-          <span className="card-title">Project Title</span>
-          <p>
-            I am a very simple card. I am good at containing small bits of
-            information. I am convenient because I require little markup to use
-            effectively.
-          </p>
-          <div className="project-summary-info">
-            <p>Posted by Onurc</p>
-            <p>December 21, 22:34</p>
-          </div>
-        </div>
-      </div>
-
-      <div className="card z-depth-1 project-summary grey darken-3 white-text">
-        <div className="card-content">
-          <span className="card-title">Project Title</span>
-          <p>
-            I am a very simple card. I am good at containing small bits of
-            information. I am convenient because I require little markup to use
-            effectively.
-          </p>
-          <div className="project-summary-info">
-            <p>Posted by Onurc</p>
-            <p>December 21, 22:34</p>
-          </div>
-        </div>
-      </div>
-
-      <div className="card z-depth-1 project-summary grey darken-3 white-text">
-        <div className="card-content">
-          <span className="card-title">Project Title</span>
-          <p>
-            I am a very simple card. I am good at containing small bits of
-            information. I am convenient because I require little markup to use
-            effectively.
-          </p>
-          <div className="project-summary-info">
-            <p>Posted by Onurc</p>
-            <p>December 21, 22:34</p>
-          </div>
-        </div>
-      </div>
-
-      <div className="card z-depth-1 project-summary grey darken-3 white-text">
-        <div className="card-content">
-          <span className="card-title">Project Title</span>
-          <p>
-            I am a very simple card. I am good at containing small bits of
-            information. I am convenient because I require little markup to use
-            effectively.
-          </p>
-          <div className="project-summary-info">
-            <p>Posted by Onurc</p>
-            <p>December 21, 22:34</p>
-          </div>
-        </div>
-      </div>
+      <ProjectSummary />
+      <ProjectSummary />
+      <ProjectSummary />
     </div>
   );
 };
---
+++ src/components/projects/ProjectSummary.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+const ProjectSummary = () => {
+  return (
+    <div className="card z-depth-1 project-summary grey darken-3 white-text">
+      <div className="card-content">
+        <span className="card-title">Project Title</span>
+        <p>
+          I am a very simple card. I am good at containing small bits of
+          information. I am convenient because I require little markup to use
+          effectively.
+        </p>
+        <div className="project-summary-info">
+          <p>Posted by Onurc</p>
+          <p>December 21, 22:34</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ProjectSummary;
