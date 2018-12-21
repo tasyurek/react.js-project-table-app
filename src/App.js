9f08d13c5c985e718baa858ee1c94b17d6b553e6
onurcantasyurek <onurcantasyurek@gmail.com>
2018-12-21 20:56:26 +0300

    install & add react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from "react";
+import { BrowserRouter } from "react-router-dom";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <h1>Project Table App</h1>
-      </div>
+      <BrowserRouter>
+        <div className="App">
+          <h1>Project Table App</h1>
+        </div>
+      </BrowserRouter>
     );
   }
 }
