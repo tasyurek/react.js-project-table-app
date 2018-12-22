d283c39f78caddb0cf510fe351d938e98c5c268b
onurcantasyurek <onurcantasyurek@gmail.com>
2018-12-23 01:28:41 +0300

    Apply ReactReduxFirebase & ReduxFirestore

--- src/config/fbConfig.js
+++ src/config/fbConfig.js
@@ -11,6 +11,5 @@ var config = {
   messagingSenderId: "853735011746"
 };
 firebase.initializeApp(config);
-firebase.firestore.settings({ timestampsInSnapshots: true });
 
 export default firebase;
--- src/index.js
+++ src/index.js
@@ -3,12 +3,22 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
-import { createStore, applyMiddleware } from "redux";
+import { createStore, applyMiddleware, compose } from "redux";
 import { Provider } from "react-redux";
 import rootReducer from "./store/reducers/rootReducer";
 import thunk from "redux-thunk";
+import { reactReduxFirebase, getFirebase } from "react-redux-firebase";
+import { reduxFirestore, getFirestore } from "redux-firestore";
+import fbConfig from "./config/fbConfig";
 
-const store = createStore(rootReducer, applyMiddleware(thunk));
+const store = createStore(
+  rootReducer,
+  compose(
+    applyMiddleware(thunk.withExtraArgument({ getFirebase, getFirestore })),
+    reactReduxFirebase(fbConfig),
+    reduxFirestore(fbConfig)
+  )
+);
 
 ReactDOM.render(
   <Provider store={store}>
