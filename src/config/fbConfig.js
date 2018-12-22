831742f65a2ac6eaf963e97d83063891ee284e98
onurcantasyurek <onurcantasyurek@gmail.com>
2018-12-23 00:46:32 +0300

    Apply Firebase

---
+++ src/config/fbConfig.js
@@ -0,0 +1,16 @@
+import firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/auth";
+
+var config = {
+  apiKey: "AIzaSyAQBgZxbC1Qhes1UL64fY5OhrvZ56YFx_U",
+  authDomain: "project-table-app.firebaseapp.com",
+  databaseURL: "https://project-table-app.firebaseio.com",
+  projectId: "project-table-app",
+  storageBucket: "project-table-app.appspot.com",
+  messagingSenderId: "853735011746"
+};
+firebase.initializeApp(config);
+firebase.firestore.settings({ timestampsInSnapshots: true });
+
+export default firebase;
