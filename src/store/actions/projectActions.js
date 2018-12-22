b421846a5a8b9a066892353f2d1a9cd939fa9511
onurcantasyurek <onurcantasyurek@gmail.com>
2018-12-23 01:45:48 +0300

    Add data to firestore

--- src/store/actions/projectActions.js
+++ src/store/actions/projectActions.js
@@ -1,6 +1,24 @@
+import { firestoreReducer } from "react-redux-firebase";
+
 export const createProject = project => {
-  return (dispatch, getState) => {
+  return (dispatch, getState, { getFirebase, getFirestore }) => {
     //TODO async call to database
-    dispatch({ type: "CREATE_PROJECT", project });
+    const firestore = getFirestore();
+
+    firestore
+      .collection("projects")
+      .add({
+        ...project,
+        authorFirstName: "Onur Can",
+        authorLastName: "Tasyurek",
+        authorId: 12345,
+        createdAt: new Date()
+      })
+      .then(() => {
+        dispatch({ type: "CREATE_PROJECT", project });
+      })
+      .catch(err => {
+        dispatch({ type: "CREATE_PROJECT_ERROE", err });
+      });
   };
 };
--- src/store/reducers/projectReducer.js
+++ src/store/reducers/projectReducer.js
@@ -24,9 +24,13 @@ const projectReducer = (state = initState, action) => {
   switch (action.type) {
     case "CREATE_PROJECT":
       console.log("project created", action.project);
+      return state;
+    case "CREATE_PROJECT_ERROR":
+      console.log("project created", action.project);
+      return state;
+    default:
+      return state;
   }
-
-  return state;
 };
 
 export default projectReducer;
